6797beeddcd817b352c743872dbf5c6576594b0a
Victor Nogueira <felladrin@gmail.com>
2020-08-16 20:02:42 +0300

    Simplify "person" game-object, to avoid using Class

--- src/ts/game-objects/person.ts
+++ src/ts/game-objects/person.ts
@@ -1,13 +1,30 @@
-import { Person } from "../classes/Person";
 import { idleText } from "./idleText";
 import { workingText } from "./workingText";
+import { Sprite, GameObject } from "kontra";
 
-export const person = new Person({
+export const person: Sprite & {
+  speed?: number;
+  targetGameObject?: GameObject;
+} = Sprite({
   x: idleText.x,
   y: idleText.y,
   color: "red",
   width: 20,
   height: 20,
-  speed: 3,
-  targetGameObject: workingText,
+  update: () => {
+    if (
+      person.targetGameObject &&
+      person.speed &&
+      person.position.distance(person.targetGameObject.position) > person.speed
+    ) {
+      const direction = person.targetGameObject.position
+        .subtract(person.position)
+        .normalize();
+      person.velocity = direction.scale(person.speed);
+      person.advance();
+    }
+  },
 });
+
+person.speed = 3;
+person.targetGameObject = workingText;
