2679ed2d3a603e9c31b582707cd34d3487bb491d
Victor Nogueira <felladrin@gmail.com>
2020-08-20 22:27:35 +0300

    Prevent iOS bounce and pinch-to-zoom

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,16 @@
+import "inobounce";
 import "./canvas";
 import { game } from "./game";
+
+document.addEventListener(
+  "touchmove",
+  function (event) {
+    const { scale } = (event as unknown) as { scale: number };
+    if (scale !== 1) {
+      event.preventDefault();
+    }
+  },
+  { passive: false }
+);
 
 game.start();
