a47913bba089a9d8a9de73cf0ba1a93c3b6a3271
Victor Nogueira <felladrin@gmail.com>
2020-08-23 15:52:24 +0300

    Install Chii for remote debugging during development

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,5 +3,15 @@ import "./canvas";
 import { game } from "./game";
 import { disablePinchToZoom } from "./functions";
 
+if (process.env.NODE_ENV === "development") {
+  try {
+    const chiiScript = document.createElement("script");
+    chiiScript.src = `//${window.location.hostname}:1233/target.js`;
+    document.head.appendChild(chiiScript);
+  } catch (error) {
+    console.error(`Chii was not initialized due to ${error}`);
+  }
+}
+
 disablePinchToZoom();
 game.start();
