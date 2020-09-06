1e7666eb1ef2e5b0b6d73a5b7211f2ee191f7cff
Victor Nogueira <felladrin@gmail.com>
2020-09-06 20:09:27 +0300

    Improve start text

--- src/ts/scenes/start/startText.ts
+++ src/ts/scenes/start/startText.ts
@@ -2,15 +2,17 @@ import { Text } from "kontra";
 import { gameWidth, gameHeight, defaultFontFamily } from "../../constants";
 import wrap from "word-wrap";
 
+const targetAmount = Number(1000000).toLocaleString();
+
 export const startText = Text({
   text: wrap(
     [
       "Earth is dying.",
       "In a joint effort from humankind, a spaceship was crafted.",
       "Large enough to transport supplies, animals and 404 astronauts, it was launched into space for a light-year trip to the nearest habitable planet: Secunda.",
-      "Get ready to conquer a new land. You can start by exploring it to find new sites for construction, then research tools, strategies and techniques to improve the work efficiency.",
-      "To guide the population, press and hold the action areas.",
-      "Their objective is to stock a million units of food and resources before the next spaceship arrives.",
+      "Get ready to conquer: You can start by exploring it to find new sites for construction, then research tools, strategies and techniques to improve the work efficiency.",
+      "Guide the population: Press and hold the action areas.",
+      `Objective: Have at least ${targetAmount} units of Food and Resources in stock before the next spaceship arrives.`,
       "Press anywhere to start.",
     ].join("\n\n"),
     { width: 60 }
