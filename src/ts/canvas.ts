4fb738ee8edae0abc3e355b6313cacc8283b028d
Victor Nogueira <felladrin@gmail.com>
2021-07-26 03:03:21 +0300

    Improve canvas resizing

--- src/ts/canvas.ts
+++ src/ts/canvas.ts
@@ -1,50 +1,36 @@
-import { init, initPointer, on } from "kontra";
+import { init, initPointer } from "kontra";
 import { gameWidth, gameHeight } from "./constants";
 import { gameStore } from "./gameStore";
-import { GameEvent } from "./enums";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import fit from "math-fit";
 
 export const { canvas } = init("gameCanvas");
 canvas.width = gameWidth;
 canvas.height = gameHeight;
 
-const lastRecordedWindowSize = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
 function handleWindowResize() {
   if (!canvas.parentElement) return;
 
-  const proportion = {
-    width:
-      Math.min(canvas.parentElement.clientWidth, lastRecordedWindowSize.width) /
-      canvas.width,
-    height:
-      Math.min(
-        canvas.parentElement.clientHeight,
-        lastRecordedWindowSize.height
-      ) / canvas.height,
-  };
-
-  const scale =
-    proportion.width < proportion.height ? proportion.width : proportion.height;
-
-  const topPercentage =
-    50 * (lastRecordedWindowSize.height / canvas.parentElement.clientHeight);
+  const fittingProps = fit.contain(
+    { w: canvas.width, h: canvas.height },
+    {
+      w: canvas.parentElement.clientWidth,
+      h: canvas.parentElement.clientHeight,
+    }
+  );
 
   const style: Partial<CSSStyleDeclaration> = {
-    position: "relative",
-    top: `${topPercentage}%`,
-    left: "50%",
-    transform: `translate(-50%, -50%) scale(${scale})`,
-    transformOrigin: "center center",
+    marginTop: `${fittingProps.top}px`,
+    marginLeft: `${fittingProps.left}px`,
+    width: `${fittingProps.width}px`,
+    height: `${fittingProps.height}px`,
   };
 
   for (const declaration of Object.keys(style)) {
-    canvas.style.setProperty(
-      declaration,
-      (style as Record<string, string>)[declaration]
-    );
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    canvas.style[declaration] = (style as Record<string, string>)[declaration];
   }
 
   window.scrollTo(1, 0);
@@ -53,24 +39,12 @@ function handleWindowResize() {
 
 handleWindowResize();
 
+window.addEventListener("resize", handleWindowResize);
+
 gameStore.on("@changed", (state) => {
   if (state.hoveredButton) {
     canvas.style.cursor = "pointer";
   } else if (state.hoveredButton === null) {
     canvas.style.cursor = "auto";
-  }
-});
-
-on(GameEvent.GameTick, () => {
-  const currentWidth = window.innerWidth;
-  const currentHeight = window.innerHeight;
-
-  if (
-    currentWidth != lastRecordedWindowSize.width ||
-    currentHeight != lastRecordedWindowSize.height
-  ) {
-    lastRecordedWindowSize.width = currentWidth;
-    lastRecordedWindowSize.height = currentHeight;
-    handleWindowResize();
   }
 });
